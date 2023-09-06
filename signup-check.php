29a300203b20079f254ba3c961f308625fc7ebd6
Toqeernaqvi <toqeernaqvi66@gmail.com>
2023-09-07 02:32:01 +0500

    add admin panel and singup functionality

--- signup-check.php
+++ signup-check.php
@@ -2,8 +2,7 @@
 session_start(); 
 include "db_conn.php";
 
-if (isset($_POST['uname']) && isset($_POST['password'])
-    && isset($_POST['name']) && isset($_POST['re_password'])) {
+if (isset($_POST['name']) && isset($_POST['password'])) {
 
 	function validate($data){
        $data = trim($data);
@@ -12,50 +11,49 @@ if (isset($_POST['uname']) && isset($_POST['password'])
 	   return $data;
 	}
 
-	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
-	$re_pass = validate($_POST['re_password']);
+	// $re_pass = validate($_POST['re_password']);
 	$name = validate($_POST['name']);
 
-	$user_data = 'uname='. $uname. '&name='. $name;
+	$user_data = 'name='. $name;
 
 
-	if (empty($uname)) {
+	if (empty($name)) {
 		header("Location: signup.php?error=User Name is required&$user_data");
 	    exit();
 	}else if(empty($pass)){
         header("Location: signup.php?error=Password is required&$user_data");
 	    exit();
 	}
-	else if(empty($re_pass)){
-        header("Location: signup.php?error=Re Password is required&$user_data");
-	    exit();
-	}
+	// else if(empty($re_pass)){
+  //       header("Location: signup.php?error=Re Password is required&$user_data");
+	//     exit();
+	// }
 
-	else if(empty($name)){
-        header("Location: signup.php?error=Name is required&$user_data");
-	    exit();
-	}
+	// else if(empty($name)){
+  //       header("Location: signup.php?error=Name is required&$user_data");
+	//     exit();
+	// }
 
-	else if($pass !== $re_pass){
-        header("Location: signup.php?error=The confirmation password  does not match&$user_data");
-	    exit();
-	}
+	// else if($pass !== $re_pass){
+  //       header("Location: signup.php?error=The confirmation password  does not match&$user_data");
+	//     exit();
+	// }
 
 	else{
 
 		// hashing the password
         $pass = md5($pass);
 
-	    $sql = "SELECT * FROM users WHERE user_name='$uname' ";
+	    $sql = "SELECT * FROM users WHERE name ='$name' ";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
 			header("Location: signup.php?error=The username is taken try another&$user_data");
 	        exit();
 		}else {
-           $sql2 = "INSERT INTO users(user_name, password, name) VALUES('$uname', '$pass', '$name')";
+           $sql2 = "INSERT INTO users(password, name) VALUES('$pass', '$name')";
            $result2 = mysqli_query($conn, $sql2);
            if ($result2) {
            	 header("Location: signup.php?success=Your account has been created successfully");
--- signup.php
+++ signup.php
@@ -27,28 +27,17 @@
                       placeholder="Name"><br>
           <?php }?>
 
-          <label>User Name</label>
-          <?php if (isset($_GET['uname'])) { ?>
-               <input type="text" 
-                      name="uname" 
-                      placeholder="User Name"
-                      value="<?php echo $_GET['uname']; ?>"><br>
-          <?php }else{ ?>
-               <input type="text" 
-                      name="uname" 
-                      placeholder="User Name"><br>
-          <?php }?>
 
 
      	<label>Password</label>
      	<input type="password" 
                  name="password" 
                  placeholder="Password"><br>
-
+<!-- 
           <label>Re Password</label>
           <input type="password" 
                  name="re_password" 
-                 placeholder="Re_Password"><br>
+                 placeholder="Re_Password"><br> -->
 
      	<button type="submit">Sign Up</button>
           <a href="index.php" class="ca">Already have an account?</a>
