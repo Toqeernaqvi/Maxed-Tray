8e21ef266a78d284236699b8d04aaebede1c1fea
Toqeernaqvi <toqeernaqvi66@gmail.com>
2023-09-15 21:17:27 +0500

    improve code

--- signup.php
+++ signup.php
@@ -21,13 +21,13 @@
           Please create your new account to continue.
         </p>
 
-        <!-- <?php if (isset($_GET['error'])) { ?>
+         <?php if (isset($_GET['error'])) { ?>
                <p class="error"><?php echo $_GET['error']; ?></p>
           <?php } ?>
 
           <?php if (isset($_GET['success'])) { ?>
                <p class="success"><?php echo $_GET['success']; ?></p>
-          <?php } ?> -->
+          <?php } ?>
           <p class="error-msg"></p>
 
         <form class="signup-form" action="signup-backend.php" method="post">
