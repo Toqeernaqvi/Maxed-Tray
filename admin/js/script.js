fea8d2c45fc5294f3135a6560647b111254afdff
Malik Haziq <malikhaziq153@gmail.com>
2023-10-05 03:12:57 +0500

    made user tab dynamic

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,7 +1,8 @@
-function openside() {
-  // if (document.getElementById("sidenav").style.width == "0px") {
-  //   document.getElementById("sidenav").style.width = "250px";
-  // } else {
-  //   document.getElementById("sidenav").style.width = "0px";
-  // }
+const usersBtn = document.querySelector('.user-tab')
+const url = document.URL
+
+if(url === 'http://localhost/Maxed-Tray/admin/users.php') {
+    usersBtn.classList.add('highlighted');
 }
+
+  
