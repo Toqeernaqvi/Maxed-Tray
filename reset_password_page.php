0110e10fe67ef36d3de33f440480f77cd7d45a64
Malik Haziq <malikhaziq153@gmail.com>
2023-10-04 06:20:12 +0500

    added reset password page

--- reset_password_page.php
+++ reset_password_page.php
@@ -7,37 +7,37 @@
   <link rel="stylesheet" type="text/css" href="./login.css">
   <script src="./src/login.js" defer></script>
 
-  <title>Login | Maxed Tray</title>
+  <title>Reset Password | Maxed Tray</title>
 </head>
 
 <body>
-  <main class="login">
+  <main class="pf">
     <!-- Show Request Response -->
     <?php if (isset($_GET['success'])) { ?> <p class="success"><?php echo $_GET['success']; ?></p> <?php } ?>
     <?php if (isset($_GET['error'])) { ?> <p class="error-msg" style="color: red;"><?php echo $_GET['error']; ?></p> <?php } ?>
 
-    <section class="form-section">
-      <h1>Login</h1>
-      <p class="login-subtitle">Login with your credential information</p>
+    <section class="fp-form-section">
+      <h1>Reset password</h1>
+      <!-- <p class="login-subtitle">Login with your credential information</p> -->
       <p class="error-msg"></p>
-      <form class="login-form" action="login.php" method="post">
+      <form class="fp-form" action="reset_password.php" method="post">
         <label>
           <p>Email</p>
           <input type="email" name="email" placeholder="Enter your email" required/>
         </label>
-        <label class="pass-container">
-          <p>Password</p>
-          <input type="password" name="password" placeholder="Create a password" required/>
+        <label>
+          <p>Registration Key</p>
+          <input type="text" name="registration_key" placeholder="Enter your key" required/>
+        </label>
+        <label class="fp-pass-container">
+          <p>New Password</p>
+          <input type="password" name="password" placeholder="Enter new password" required/>
           <img src="./assets/icons/eye.svg" alt="eye icon" class="open-eye">
           <img src="./assets/icons/close-eye.svg" alt="close eye icon" class="close-eye hidden">  
           </div>
         </label>
-        <a href="reset_password_page.php" class="forgot">Forgot Password?</a>
-        <button type="submit">Login</button>
+        <button type="submit">Reset</button>
       </form>
-      <p class="signup-link">
-        Don't have an account? <a href="signup.php">Sign up</a>
-      </p>
     </section>
   </main>
 </body>
